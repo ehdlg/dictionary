f813911e459cceeadd2f198827628f5e3a56ce09
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-07-17 16:45:37 +0200

    feat(interfaces): update interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -2,23 +2,33 @@ export interface Dictionary {
   word: string;
   phonetic: string;
   phonetics: Phonetic[];
-  origin: string;
   meanings: Meaning[];
+  license: License;
+  sourceUrls: string[];
+}
+
+export interface License {
+  name: string;
+  url: string;
 }
 
 export interface Meaning {
   partOfSpeech: string;
   definitions: Definition[];
+  synonyms: string[];
+  antonyms: any[];
 }
 
 export interface Definition {
   definition: string;
-  example: string;
   synonyms: any[];
   antonyms: any[];
+  example?: string;
 }
 
 export interface Phonetic {
   text: string;
-  audio?: string;
+  audio: string;
+  sourceUrl?: string;
+  license?: License;
 }
