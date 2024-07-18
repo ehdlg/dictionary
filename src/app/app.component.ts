4201818839dc874c0631aecaa9738b22736a18b7
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-07-18 09:17:29 +0200

    feat(refactor): refactor app component adding search and word child components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DictionaryService } from './dictionary.service';
 import { Dictionary } from '../../interfaces';
 import { AsyncPipe, JsonPipe } from '@angular/common';
 import { Observable } from 'rxjs';
 import { FormsModule } from '@angular/forms';
 import { WordComponent } from './word/word.component';
+import { SearchComponent } from './search/search.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AsyncPipe, JsonPipe, FormsModule, WordComponent],
+  imports: [
+    AsyncPipe,
+    JsonPipe,
+    FormsModule,
+    WordComponent,
+    SearchComponent,
+    HeaderComponent,
+  ],
   template: `
     <main class="flex max-w-xl mx-auto flex-col gap-8 m-16">
-      <h1 class="text-center text-3xl font-bold text-underline text-slate-800">
-        Dictionary!
-      </h1>
-      <form (submit)="onSubmit()">
-        <input
-          [(ngModel)]="word"
-          type="text"
-          id="word"
-          class="w-full bg-gray-50 border border-transparent rounded-xl outline-none text-2xl mx-auto p-3 focus:shadow-md transition ease-in duration-250"
-          [ngModelOptions]="{ standalone: true }"
-        />
-        <button class="hidden" type="submit">Submit</button>
-      </form>
-
-      <app-word [word]="sendWord" />
+      <app-header />
+      <app-search (wordEvent)="updateWord($event)" />
+      <app-word [data$]="data$" />
     </main>
   `,
   styles: [],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  static DEFAULT_WORD = 'keyboard';
   public data$!: Observable<Dictionary>;
-  public word = 'keyboard';
-  protected sendWord: string | null = null;
+  public word: string;
 
-  constructor(private service: DictionaryService) {}
+  constructor(private service: DictionaryService) {
+    this.word = AppComponent.DEFAULT_WORD;
+  }
 
-  onSubmit() {
-    this.data$ = this.service.get(this.word);
-    this.sendWord = this.word;
+  private updateWordObservable(newWord: string) {
+    this.data$ = this.service.get(newWord);
     this.word = '';
+  }
+
+  ngOnInit() {
+    this.updateWordObservable(this.word);
+  }
+
+  updateWord(newWord: string) {
+    this.updateWordObservable(newWord);
   }
 }
